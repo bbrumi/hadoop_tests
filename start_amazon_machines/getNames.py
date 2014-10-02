4c92ba79124e52c9e2c474e28e7af58c99cf77b0
Tunde <tunde.balint@gmail.com>
2014-10-02 15:17:29 +0200

    snapshots

--- start_amazon_machines/getNames.py
+++ start_amazon_machines/getNames.py
@@ -3,16 +3,18 @@
 import boto.ec2
 import sys
 
-nr_workers=5
-nr_masters=2
+nr_workers=4
+nr_masters=1
 
 worker_type='m1.small'
 master_type='m1.medium'
 
+hosts=['elephant', 'tiger', 'monkey', 'horse', 'lion']
+
 my_masters_list=[]
 my_workers_list=[]
 
-nr_clients=2
+nr_clients=1
 
 conn=boto.ec2.connect_to_region("eu-west-1")
 reservations = conn.get_all_instances()
@@ -28,16 +30,7 @@ worker_cnt=0
 master_cnt=0
 for i in range(nr_clients):
 	print 'Client %s' % i
-	print 'Masters:'	
-	for j in range(nr_masters):
-		if (master_cnt >= len(my_masters_list)):
-			print 'Not enough master instances'
-			sys.exit(1) 
-		master = my_masters_list[master_cnt] 
-		master_cnt+=1
-		#print "%s %s %s %s" % (master.id, master.public_dns_name, master.private_dns_name, master.instance_type)
-		print "%s %s" % (master.ip_address, master.private_ip_address)
-  	print 'Workers:'
+        nr = 0	
 	for j in range(nr_workers):      
                 if (worker_cnt >= len(my_workers_list)):
                         print 'Not enough worker instances'
@@ -45,4 +38,15 @@ for i in range(nr_clients):
                 worker = my_workers_list[worker_cnt] 
                 worker_cnt+=1
                 #print "%s %s %s %s" % (worker.id, worker.public_dns_name, worker.private_dns_name, worker.instance_type)
-		print "%s %s" % (master.ip_address, master.private_ip_address)
+		print "%s : %s %s" % (hosts[nr], worker.ip_address, worker.private_ip_address)
+		nr+=1
+        for j in range(nr_masters):
+                if (master_cnt >= len(my_masters_list)):
+                        print 'Not enough master instances'
+                        sys.exit(1) 
+                master = my_masters_list[master_cnt] 
+                master_cnt+=1
+                #print "%s %s %s %s" % (master.id, master.public_dns_name, master.private_dns_name, master.instance_type)
+                print "%s : %s %s" % (hosts[nr], master.ip_address, master.private_ip_address)
+		nr+=1
+
