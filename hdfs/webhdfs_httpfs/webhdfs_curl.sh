17845df4167429fc429611369b1cd40a7e5b7014
Tunde <tunde.balint@gmail.com>
2014-10-02 21:38:52 +0200

    webhdfs

--- hdfs/webhdfs_httpfs/webhdfs_curl.sh
+++ hdfs/webhdfs_httpfs/webhdfs_curl.sh
@@ -2,62 +2,81 @@
 
 # default HttpFS port 14000
 # WehHDFS port - NN port - default: 50070
+DIR=tmp
+FILE=testWebHDFS.txt
+USER=training
 
 USE="WebHDFS: $0 <Active Namenode:Port> [status|put|get|mkdir|deleteFir|deleteDir] \n
      HttpFS: $0 <HTTPServer:Port> [status|put|get|mkdir|deleteFir|deleteDir] \n"
 
-if [ $# -ne 2 ]; 
-    then 
-	echo "illegal number of parameters"
-	echo $USE
-	exit 0
+if [ $# -lt 2 ] || [ $# -gt 4 ];
+    then
+        echo "illegal number of parameters"
+        echo $USE
+        exit 0
 fi
-
 
 SERVER_AND_PORT=$1
 CMD=$2
 QUERY="http://$SERVER_AND_PORT/webhdfs/v1"
 
-USER=training
+if [ $# -eq 4 ];
+        then
+                DIR=$3
+                FILE=$4
+fi
 
-DIR=testWebHDFS
-FILE=testWebHDFS.txt
+if [ $# -eq 3 ];
+        then
+                DIR=$3
+fi
 
+DIR="user/$USER/$DIR"
 
 case $CMD in
-	status)
-		echo "Get $TESTDIR directory status"
-		curl -i "$QUERY/$DIR?op=LISTSTATUS&user.name=$USER"
-	;;
+        status)
+                echo "Get $DIR directory status"
+                curl -i "$QUERY/$DIR?op=LISTSTATUS&user.name=$USER"
+        ;;
         mkdir)
-                echo "Create $TESTDIR directory"
-                curl -i -X PUT "$QUERY/$TESTDIR/?op=MKDIRS&user.name=$USER"
+                echo "Create $DIR directory"
+                curl -i -X PUT "$QUERY/$DIR/?op=MKDIRS&user.name=$USER"
         ;;
         put)
-                # Some options:
+                # test if directory exists
+                exists=`curl -i  "$QUERY/$DIR?op=GETFILESTATUS&user.name=$USER" 2>/dev/null| grep 'FileNotFoundException'`
+                if [ -z "$exists" ];
+                        then
+                                echo "Directory exists..."
+                        else:
+                                echo "Directory does not exist...creating $DIR"
+                                curl -i -X PUT "$QUERY/$DIR/?op=MKDIRS&user.name=$USER"
+                fi
+                echo "Will now PUT $FILE into directory $DIR"
                 # curl -i -X PUT "http://<HOST>:<PORT>/webhdfs/v1/<PATH>?op=CREATE
                 #                [&overwrite=<true|false>][&blocksize=<LONG>][&replication=<SHORT>]
                 #                [&permission=<OCTAL>][&buffersize=<INT>]"
-                # TODO: Test that the TestDir already exists
-		# test if directory exists
-		exists=`curl -i  "http://$QUERY/webhdfs/v1/$TESTDIR?op=GETFILESTATUS" | grep 'FileNotFoundException'`
-		echo $exists
-                echo "Will now PUT ./testdata.txt into directory $TESTDIR"
-                curl -i -L -X PUT "$QUERY/$TESTDIR/$FILE?op=CREATE&user.name=$USER" -T ./$FILE
+                curl -i -L -X PUT "$QUERY/$DIR/$FILE?op=CREATE&user.name=$USER" -T ./$FILE
         ;;
         get)
-                echo "GET $TESTDIR/$FILE"
-                curl -i "$QUERY/$TESTDIR/$FILE?op=OPEN&user.name=$USER"
+                echo "GET $DIR/$FILE"
+                redir=`curl -i "$QUERY/$DIR/$FILE?op=OPEN&user.name=$USER" 2>/dev/null`
+                echo "First we get a redirect: $redir"
+                echo "==========================="
+                echo "And now get the content of $FILE: "
+                echo
+                location=`echo "$redir" | grep "Location: http" | awk '{split($0,a,": "); print a[2] a[2]}'`
+                curl -i $location
         ;;
         deleteFile)
-                echo "Delete $TESTDIR/$FILE"
-                curl -i -X DELETE "$QUERY/$TESTDIR/$FILE?op=DELETE&user.name=$USER"
+                echo "Delete $DIR/$FILE"
+                curl -i -X DELETE "$QUERY/$DIR/$FILE?op=DELETE&user.name=$USER"
         ;;
         deleteDir)
-                echo "Delete directory $TESTDIR"
-                curl -i -X DELETE "$QUERY/$TESTDIR?op=DELETE&user.name=$USER"
+                echo "Delete directory $DIR"
+                curl -i -X DELETE "$QUERY/$DIR?op=DELETE&user.name=$USER"
                 # Try &recursive=true if directory non-empty
-                # curl -i -X DELETE "$PREFIX/$TESTDIR?op=DELETE&user.name=$USER&recursive=true"
+                # curl -i -X DELETE "$PREFIX/$DIR?op=DELETE&user.name=$USER&recursive=true"
         ;;
         *)
                 echo "Command not understood: $CMD"
@@ -65,3 +84,4 @@ case $CMD in
                 exit 1
         ;;
 esac
+
