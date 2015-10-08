e14f452c612a8060af60385d683a43fdba3c6729
Tunde Alkemade <tundealkemade@godatadriven.com>
2015-10-08 19:43:15 +0200

    changes to startup script

--- start_amazon_machines/getNames.py
+++ start_amazon_machines/getNames.py
@@ -4,17 +4,18 @@ import boto.ec2
 import sys
 
 nr_workers=4
-nr_masters=1
+#nr_masters=1
 
 worker_type='m1.small'
-master_type='m1.medium'
+#master_type='m1.medium'
 
-hosts=['elephant', 'tiger', 'monkey', 'horse', 'lion']
+#hosts=['elephant', 'tiger', 'monkey', 'horse', 'lion']
+hosts=['elephant', 'tiger', 'horse', 'monkey']
 
-my_masters_list=[]
+#my_masters_list=[]
 my_workers_list=[]
 
-nr_clients=1
+nr_clients=13
 
 conn=boto.ec2.connect_to_region("eu-west-1")
 reservations = conn.get_all_instances()
@@ -40,13 +41,13 @@ for i in range(nr_clients):
                 #print "%s %s %s %s" % (worker.id, worker.public_dns_name, worker.private_dns_name, worker.instance_type)
 		print "%s : %s %s" % (hosts[nr], worker.ip_address, worker.private_ip_address)
 		nr+=1
-        for j in range(nr_masters):
-                if (master_cnt >= len(my_masters_list)):
-                        print 'Not enough master instances'
-                        sys.exit(1) 
-                master = my_masters_list[master_cnt] 
-                master_cnt+=1
+ #       for j in range(nr_masters):
+  #              if (master_cnt >= len(my_masters_list)):
+   #                     print 'Not enough master instances'
+    #                    sys.exit(1) 
+     #           master = my_masters_list[master_cnt] 
+      #          master_cnt+=1
                 #print "%s %s %s %s" % (master.id, master.public_dns_name, master.private_dns_name, master.instance_type)
-                print "%s : %s %s" % (hosts[nr], master.ip_address, master.private_ip_address)
-		nr+=1
+       #         print "%s : %s %s" % (hosts[nr], master.ip_address, master.private_ip_address)
+	#	nr+=1
 
